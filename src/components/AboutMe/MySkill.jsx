98b1156add962bdc87273d53ec9998fc07bf4c1a
NaufalAssyafiqF <assyafiqnaufal625@gmail.com>
2025-01-01 22:16:59 +0700

    change marquee to reactfastmarquee

--- src/components/AboutMe/MySkill.jsx
+++ src/components/AboutMe/MySkill.jsx
@@ -2,7 +2,7 @@
 import { mySkills1, mySkills2 } from "@/data/skills";
 import { DesktopTower } from "@phosphor-icons/react/dist/ssr";
 import React from "react";
-import { motion } from "motion/react";
+import Marquee from "react-fast-marquee";
 
 const MySkill = () => {
   return (
@@ -13,93 +13,27 @@ const MySkill = () => {
           My Skills
         </h1>
       </div>
-      <div className="w-[90%] mx-auto overflow-hidden">
-        <div className="flex  mt-10 myGradient">
-          <motion.div
-            initial={{ x: 0 }}
-            animate={{ x: "-100%" }}
-            transition={{ duration: 30, repeat: Infinity, ease: "linear" }}
-            className="flex flex-shrink-0  w-full"
-          >
-            {mySkills1.map((item, index) => (
-              <div
-                key={index}
-                className="w-[200px] flex flex-col items-center space-x-2 justify-between"
-              >
-                <img
-                  src={item.img}
-                  alt="icon skill"
-                  className="min-w-[60px] max-w-[70px]"
-                />
-                {/* <p className="font-normal text-xl">{item.name}</p> */}
-              </div>
-            ))}
-          </motion.div>
-          <motion.div
-            initial={{ x: 0 }}
-            animate={{ x: "-100%" }}
-            transition={{ duration: 30, repeat: Infinity, ease: "linear" }}
-            className="flex flex-shrink-0  w-full"
-          >
-            {mySkills1.map((item, index) => (
-              <div
-                key={index}
-                className="w-[200px] flex flex-col items-center space-x-2 justify-between"
-              >
-                <img
-                  src={item.img}
-                  alt="icon skill"
-                  className="min-w-[60px] max-w-[70px]"
-                />
-                {/* <p className="font-normal text-xl">{item.name}</p> */}
-              </div>
-            ))}
-          </motion.div>
-        </div>
-
-        {/* slider skill 2 */}
-        <div className="flex  mt-10 myGradient">
-          <motion.div
-            initial={{ x: "-100%" }}
-            animate={{ x: 0 }}
-            transition={{ duration: 30, repeat: Infinity, ease: "linear" }}
-            className="flex flex-shrink-0  w-full"
-          >
-            {mySkills2.map((item, index) => (
-              <div
-                key={index}
-                className="w-[200px] flex flex-col items-center space-x-2 justify-between"
-              >
-                <img
-                  src={item.img}
-                  alt="icon skill"
-                  className="min-w-[60px] max-w-[70px]"
-                />
-                {/* <p className="font-normal text-xl">{item.name}</p> */}
-              </div>
-            ))}
-          </motion.div>
-          <motion.div
-            initial={{ x: "-100%" }}
-            animate={{ x: 0 }}
-            transition={{ duration: 30, repeat: Infinity, ease: "linear" }}
-            className="flex flex-shrink-0  w-full"
-          >
-            {mySkills2.map((item, index) => (
-              <div
-                key={index}
-                className="w-[200px] flex flex-col items-center space-x-2 justify-between"
-              >
-                <img
-                  src={item.img}
-                  alt="icon skill"
-                  className="min-w-[60px] max-w-[70px]"
-                />
-                {/* <p className="font-normal text-xl">{item.name}</p> */}
-              </div>
-            ))}
-          </motion.div>
-        </div>
+      <div className="w-[90%] mx-auto overflow-hidden myGradient mt-8">
+        <Marquee className="flex justify-between" direction="left">
+          {mySkills1.map((item, index) => (
+            <img
+              key={index}
+              src={item.img}
+              alt="icon skill"
+              className="min-w-[60px] max-w-[70px] md:mx-6 sm:mx-4 mx-2"
+            />
+          ))}
+        </Marquee>
+        <Marquee className="flex justify-between mt-8" direction="right">
+          {mySkills2.map((item, index) => (
+            <img
+              key={index}
+              src={item.img}
+              alt="icon skill"
+              className="min-w-[60px] max-w-[70px] md:mx-6 sm:mx-4 mx-2"
+            />
+          ))}
+        </Marquee>
       </div>
     </div>
   );
